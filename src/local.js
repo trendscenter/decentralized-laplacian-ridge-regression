63dad07d3b7863e6095ef4d3af9f9bd634d279f3
Cory Reed <creed@mrn.org>
2016-08-14 19:57:50 -0600

    Implement input feature selection in pre-processing.

--- src/local.js
+++ src/local.js
@@ -14,15 +14,53 @@ module.exports = {
     return n.transpose(withBias);
   },
 
+  /**
+   * Pre-process.
+   *
+   * @param {Object} opts
+   * @param {RemoteComputationResult} opts.remoteResult
+   * @param {Project} opts.userData
+   * @returns {Object}
+   */
   preprocess(opts) {
+    const features = get(opts, 'remoteResult.pluginState.inputs[0][0]');
+
+    if (!features || !Array.isArray(features) || !features.length) {
+      throw new Error('Expected inputs containing features');
+    } else if (
+      features.some(f => FreeSurfer.validFields.indexOf(f) < 0)
+    ) {
+      throw new Error(`Unknown FreeSurfer region in inputs: ${features.toString()}`);
+    }
+
+    /**
+     * @todo: This uses only one feature. Change to use multiple!
+     *
+     * @param {FreeSurfer} freeSurfer
+     * @returns {Number}
+     */
+    function pickFeatures(freeSurfer) {
+      return freeSurfer[features[0]];
+
+      // return Object.keys(freeSurfer).reduce((memo, key) => {
+      //   return features.indexOf(key) > -1 ?
+      //     memo.concat(freeSurfer[key]) :
+      //     memo;
+      // }, []);
+    }
+
     const userData = opts.userData;
     // do some file parsing. here, we're actually using pre-processed data...
     // so just add some bias and bail!
     const result = {};
-    const X = userData.files
-    .map((file) => fs.readFileSync(file.filename).toString())
-    .map((strSurf) => new FreeSurfer({ string: strSurf }))
-    .map((surf) => surf.TotalGrayVol); // <== feature to assess!
+    const X = userData.files.map(file => {
+      const surf = new FreeSurfer({
+        string: fs.readFileSync(file.filename).toString(),
+      });
+
+      // Pluck features to assess
+      return pickFeatures(surf);
+    });
     result.biasedX = this.addBias(n.transpose([X]));
     result.y = userData.files.map((file) => (file.tags.isControl ? 1 : -1));
     result.lambda = userData.lamba || 0.0;
