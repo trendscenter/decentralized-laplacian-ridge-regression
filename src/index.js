347cf6529e746988fa941a467eb00e7c23bec716
Jing Ming <jming@mrn.org>
2017-01-09 13:41:17 -0700

    add goodness of fit and P value for regression

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const FreeSurfer = require('freesurfer-parser');
 const pkg = require('../package.json');
 const localRunner = require('./local');
 const remoteRunner = require('./remote');
+const regression = require('./regression');
+const n = require('numeric');
+const distributions = require('distributions'); 
 const DEFAULT_MAX_ITERATIONS = require('./constants').DEFAULT_MAX_ITERATIONS;
 
 module.exports = {
@@ -45,13 +48,210 @@ module.exports = {
       }],
     }, {
       type: 'function',
-      fn(opts) { return localRunner.run(opts); },
+      fn(opts) { 
+      // if W doesn't merge, return run(), if W merge, return statistic()
+//        console.log('opts.remoteResult.data is',opts.remoteResult.data);
+//         console.log('opts.previousData is', opts.previousData);
+       console.log('opts.remoteResult.data.endOfIteration is' , opts.remoteResult.data.endOfIteration);
+       console.log('opts.remoteResult.data.statisticStep is', opts.remoteResult.data.statisticStep);
+        
+        if ((opts.remoteResult.data.endOfIteration == true) && (opts.remoteResult.data.statisticStep == 0)){
+          
+          // step 0 calculate local statistics and localYMean
+           const biasedX = opts.previousData.biasedX;
+           const y = opts.previousData.y;
+           const localCount = y.length;
+           const betaVector = regression.oneShot(biasedX, y);
+           const rSquared = regression.rSquared(biasedX, y, betaVector);
+           const tValue = regression.tValue(biasedX, y, betaVector);
+           var tdist = distributions.Studentt(localCount-1);
+           const tcdf = tValue.map(r => tdist.cdf(r));
+           const pValue = n.mul(2,n.sub(1,tcdf));
+           const localMeanY = n.sum(y) / localCount;
+ 
+           return {
+             betaVector,
+             localCount,
+             localMeanY,
+             rSquared,
+             tValue,
+             pValue,
+             biasedX,
+             y,
+             endOfIteration : true,
+             statisticStep : 0,
+          };          
+        }
+        else if ((opts.remoteResult.data.endOfIteration == true) && (opts.remoteResult.data.statisticStep == 1)){   
+           // step 1 receive the globalMeanY and currW, then calculate sseLocal, sstLocal and varXLocal
+ 
+           const globalMeanY = opts.remoteResult.data.globalMeanY;
+           const currW = opts.remoteResult.data.currW;
+           const biasedX = opts.previousData.biasedX;
+           const y = opts.previousData.y;
+           const rSquared = opts.previousData.rSquared;
+           const tValue = opts.previousData.tValue;
+           const pValue = opts.previousData.pValue;
+           const betaVector = opts.previousData.betaVector;
+           const localCount = y.length;
+
+
+          //calculate the local r squred and t value for averageBetaVector)
+           const rSquaredLocal = regression.rSquared(biasedX, y, currW);
+           const tValueLocal = regression.tValue(biasedX, y, currW);
+           var tdist=distributions.Studentt(localCount-1);
+           const tcdf = tValueLocal.map(r => tdist.cdf(r));
+           const pValueLocal = n.mul(2,n.sub(1,tcdf));
+
+          // calculate sseLocal and sstLocal
+           const sseLocal=n.sum(n.pow(n.sub(y, n.dot(biasedX, currW)), 2));
+           const sstLocal=n.sum(n.pow(n.sub(y, n.rep(n.dim(y), globalMeanY)), 2));
+
+          // calculate varXLocal
+           const varXLocalMatrix=n.dot(n.transpose(biasedX),biasedX);
+           const varXLocal=[];
+           for (let i=0; i<currW.length; i += 1) {
+             varXLocal.push(varXLocalMatrix[i][i]);
+          }  
+
+          /* eslint-disable no-console */
+           console.log('local r squared for currW', rSquaredLocal);
+           console.log('local t Values for currW', tValueLocal);
+           console.log('local p Values for currW', pValueLocal);
+          /* eslint-enable no-console */
+
+           return {
+             sseLocal,
+             sstLocal,
+             varXLocal,
+             currW,
+             localCount,
+             rSquared,
+             tValue,
+             pValue,
+             rSquaredLocal,
+             tValueLocal,
+             pValueLocal,
+             endOfIteration : true,
+             statisticStep : 1,
+          };
+        }
+        else {
+        return localRunner.run(opts);        
+       }
+      },
       verbose: true,
     },
   ],
   remote: {
     type: 'function',
-    fn(opts) { return remoteRunner.run(opts); },
+    fn(opts) { 
+
+     console.log('opts.userResults[0].data.endOfIteration is ', opts.userResults[0].data.endOfIteration);
+     console.log('opts.userResults[0].data.statisticStep is ', opts.userResults[0].data.statisticStep);
+     
+     if ((opts.userResults[0].data.endOfIteration == true) && (opts.userResults[0].data.statisticStep == 0)){
+         // if statisticStep==0, remote node calculates the globalMeanY and return it to local node
+         const userResults = opts.userResults;
+
+         // Not all user results contain betas. Return early.
+         if (userResults.some(userResult => !((userResult || {}).data || {}).betaVector)) {
+            return {};
+           }
+
+         // calculate averageBetaVector
+         const currW = opts.previousData.currW;
+
+         // calculate globalMeanY
+         const siteCount=userResults.length;
+         var totalY=0;
+         var globalYCount=0;
+
+         for (let i=0; i < siteCount; i += 1) {
+            totalY += userResults[i].data.localMeanY*userResults[i].data.localCount;
+            globalYCount += userResults[i].data.localCount;
+         }
+
+         const globalMeanY = totalY/globalYCount;
+
+         /* eslint-disable no-console */
+         console.log('currW :', currW);
+         console.log('globalMeanY is :', globalMeanY);
+         /* eslint-enable no-console */
+   
+           
+ 
+         return { currW,
+                 globalMeanY,
+                 endOfIteration : true,
+                 statisticStep : 1,
+                };
+
+     }
+    else if ((opts.userResults[0].data.endOfIteration == true) && (opts.userResults[0].data.statisticStep == 1)) {
+         // if statisticStep==1, remote node calculates the global statistics 
+         
+         const userResults = opts.userResults;
+
+        // get passed parameters from local nodes
+         const currW = opts.previousData.currW;
+         const betaVectorLocal = userResults.map(r => r.data.betaVector);
+         const rSquaredLocalOriginal = userResults.map(r => r.data.rSquared);
+         const tValueLocalOriginal = userResults.map(r => r.data.tValue);
+         const pValueLocalOriginal = userResults.map(r => r.data.pValue);
+         const rSquaredLocal = userResults.map(r => r.data.rSquaredLocal);
+         const tValueLocal = userResults.map(r => r.data.tValueLocal);
+         const pValueLocal = userResults.map(r => r.data.pValueLocal);
+
+        //calculate global parameters
+         const sseGlobal = userResults.reduce((sum, userResult) => sum + userResult.data.sseLocal, 0);
+         const sstGlobal = userResults.reduce((sum, userResult) => sum + userResult.data.sstLocal, 0);
+         const globalYCount = userResults.reduce((sum,userResult) => sum + userResult.data.localCount,0);
+         const betaCount = currW.length;
+         const varError = (1 /(globalYCount-2)) * sseGlobal;
+         const varXGlobal = [];
+         const seBetaGlobal = [];
+         const tValueGlobal = [];
+ 
+        // calculate tValueGlobal
+         for (let i = 0; i < betaCount; i += 1) {
+           varXGlobal[i] = userResults.reduce((sum, userResult) => sum + userResult.data.varXLocal[i], 0 );
+           seBetaGlobal[i] = Math.sqrt(varError/varXGlobal[i]);
+           tValueGlobal[i] = currW[i]/seBetaGlobal[i];
+          }  
+
+       // calculate r squared global
+         const rSquaredGlobal = 1-(sseGlobal/sstGlobal);
+
+       // add t to p value transformation //
+         var tdist=distributions.Studentt(globalYCount-1);
+         const tcdf = tValueGlobal.map(r => tdist.cdf(r));
+         const pValueGlobal=n.mul(2,(n.sub(1,tcdf))); // two tail pValue
+
+         console.log('The global r squared for currW :', rSquaredGlobal);
+         console.log('The global t Values for currW :', tValueGlobal);
+         console.log('The global p Values for currW :', pValueGlobal);
+
+         return {
+        betaVectorLocal,
+        currW,
+        rSquaredLocalOriginal,
+        tValueLocalOriginal,
+        pValueLocalOriginal,
+        rSquaredLocal,
+        tValueLocal,
+        pValueLocal,
+        rSquaredGlobal,
+        tValueGlobal,
+        pValueGlobal,
+        complete: true,
+      };
+
+         }
+     else {
+      return remoteRunner.run(opts); 
+     }
+    },
     verbose: true,
   },
 };
